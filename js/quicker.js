67203fdfa0dcee2b50edb25f2c1c8954722702ab
LuisArmando-TestCoder <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2018-11-18 12:17:41 -0600

    LuisArmando-TestCoder´s library

---
+++ js/quicker.js
@@ -0,0 +1,12 @@
+function qs(param){
+  return document.querySelector(param);
+}
+function qsa(param) {
+  return document.querySelectorAll(param);
+}
+function gc(param) {
+  return document.getElementsByClassName(param);
+}
+function gi(param) {
+  return document.getElementById(param);
+}
